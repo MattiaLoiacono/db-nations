8ad9f31af1dd30f2e23b08701e3856fcbf85f061
MattiaLoiacono <93258667+MattiaLoiacono@users.noreply.github.com>
2021-12-20 12:24:58 +0100

    bonus

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 	
 	private final static String URL = "jdbc:mysql://localhost:3306/db-nations";
 	private final static String USER = "root";
-	private final static String PASSWORD = "rootpassword";
+	private final static String PASSWORD = "";
 
 	public static void main(String[] args) {
 		
@@ -20,9 +20,9 @@ public class Main {
 			String search = scan.nextLine();
 			search = "%" + search + "%";
 			
-			String query = "select c.country_id , c.name , r.name , c2.name from countries c join regions r on r.region_id = c.region_id join continents c2 on c2.continent_id = r.continent_id where c.name like ? order by c.name ;";
+			String query1 = "select c.country_id , c.name , r.name , c2.name from countries c join regions r on r.region_id = c.region_id join continents c2 on c2.continent_id = r.continent_id where c.name like ? order by c.name ;";
 			
-			try(PreparedStatement ps = con.prepareStatement(query)){
+			try(PreparedStatement ps = con.prepareStatement(query1)){
 				ps.setString(1, search);
 				try(ResultSet rs = ps.executeQuery()){
 					
@@ -40,6 +40,38 @@ public class Main {
 						throw new Exception("No results");
 					}
 				}
+				
+				System.out.print("Choose a country id: ");
+				int id = scan.nextInt();
+				String query2 = "select c.name , l.`language`, max(cs.`year`), cs.population , cs.gdp from countries c join country_stats cs on cs.country_id = c.country_id join country_languages cl on cl.country_id = cs.country_id join languages l on l.language_id = cl.language_id where c.country_id = ? group by l.language_id;";
+				
+				try(PreparedStatement ps2 = con.prepareStatement(query2)){
+					ps2.setInt(1, id);
+					try(ResultSet rs2 = ps2.executeQuery()){
+						
+						if(rs2.next()) {
+							System.out.println("Details for country: " + rs2.getString(1));
+							System.out.print("Languages: ");
+							
+							do {
+								System.out.print(rs2.getString(2));
+								if(!rs2.isLast()) {
+									System.out.print(", ");
+								}else {
+									
+									System.out.println("%nMost recent stats");
+									System.out.println("Year: "+ rs2.getInt(3));
+									System.out.println("Population: "+ rs2.getLong(4));
+									System.out.println("GDP: "+ rs2.getLong(5));
+								}
+							}while(rs2.next());
+							
+						}else {
+							throw new Exception("Invalid ID");
+						}
+					}
+				}
+				
 			}
 			
 		}catch(SQLException s){
